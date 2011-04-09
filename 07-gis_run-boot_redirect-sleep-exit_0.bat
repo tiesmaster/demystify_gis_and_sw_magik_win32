2516db5b67baac951b0ad9460844ce941e3e5573
Thijs Brobbel <thijsbrobbel@gmail.com>
2011-04-09 18:37:28 +0200

    test case 7: git run using the SW method using start, and "boot redirect"

---
+++ 07-gis_run-boot_redirect-sleep-exit_0.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set title=magik_image: swaf
+set gis=start /wait /min "%title%" cmd /c gis.exe
+
+%gis% -l output.log swaf ^< 07-gis_run-boot_redirect-sleep-exit_0.magik
+echo %%ERRORLEVEL%% == %ERRORLEVEL%
+copy output.log con
+del output.log
