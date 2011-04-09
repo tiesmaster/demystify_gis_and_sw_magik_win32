575f14f6a0c645030f7a7730306d3497fe4866c7
Thijs Brobbel <thijsbrobbel@gmail.com>
2011-04-09 18:17:34 +0200

    refactored test cases according to naming convention

--- 05-gis_run-sleep-exit_0.bat
+++ 05-gis_run-sleep-exit_0.bat
@@ -1,5 +1,5 @@
 @echo off
-gis -l output.log swaf <06-gis_run_sleep-exit_1.magik
+gis -l output.log swaf <05-gis_run-sleep-exit_0.magik
 echo %%ERRORLEVEL%% == %ERRORLEVEL%
 copy output.log con
 del output.log
