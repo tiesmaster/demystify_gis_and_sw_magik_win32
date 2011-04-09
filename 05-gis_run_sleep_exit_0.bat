0821a0745ecff487d536f2a4489130bf9ca856a2
Thijs Brobbel <thijsbrobbel@gmail.com>
2011-04-09 18:05:51 +0200

    refactored test case names

--- 05-gis_run_sleep_exit_0.bat
+++ 05-gis_run_sleep_exit_0.bat
@@ -1,5 +1,5 @@
 @echo off
-gis -l output.log swaf <06-normal_gis_run_sleep_exit_1.magik
+gis -l output.log swaf <05-gis_run_sleep_exit_0.magik
 echo %%ERRORLEVEL%% == %ERRORLEVEL%
 copy output.log con
 del output.log
--- 06-gis_run_sleep_exit_1.bat
+++ 06-gis_run_sleep_exit_1.bat
@@ -1,5 +1,5 @@
 @echo off
-gis -l output.log swaf <05-normal_gis_run_sleep.magik
+gis -l output.log swaf <06-gis_run_sleep_exit_1.magik
 echo %%ERRORLEVEL%% == %ERRORLEVEL%
 copy output.log con
 del output.log
