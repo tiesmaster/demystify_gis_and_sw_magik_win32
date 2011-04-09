7388f29a8d1121b6bc8d9b29e79cd6a6f25e38e9
Thijs Brobbel <thijsbrobbel@gmail.com>
2011-04-09 18:39:00 +0200

    test case 08: like 8, but with exit code 1

---
+++ 08-gis_run-boot_redirect-sleep-exit_1.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set title=magik_image: swaf
+set gis=start /wait /min "%title%" cmd /c gis.exe
+
+%gis% -l output.log swaf ^< 08-gis_run-boot_redirect-sleep-exit_1.magik
+echo %%ERRORLEVEL%% == %ERRORLEVEL%
+copy output.log con
+del output.log
