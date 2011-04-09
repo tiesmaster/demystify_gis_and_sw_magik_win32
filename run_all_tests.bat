325a57f63118693a0cdd067265f5f8cf04d1b947
Thijs Brobbel <thijsbrobbel@gmail.com>
2011-04-09 18:48:00 +0200

    added script for running all tests

---
+++ run_all_tests.bat
@@ -0,0 +1,8 @@
+call 01-normal_gis_run-exit_0.bat
+call 02-normal_gis_run-exit_1.bat
+call 03-sw_magik_win32_run-exit_0.bat
+call 04-sw_magik_win32_run-exit_1.bat
+call 05-gis_run-sleep-exit_0.bat
+call 06-gis_run-sleep-exit_1.bat
+call 07-gis_run-boot_redirect-sleep-exit_0.bat
+call 08-gis_run-boot_redirect-sleep-exit_1.bat
